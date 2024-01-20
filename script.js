426fde38a7f477698f73dbbcbff4774e42a749bc
Antony Petsas <152808269+TKP888@users.noreply.github.com>
2024-01-20 14:32:53 +0000

    scoring functionality now working
    need to update random selection to manual for player

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const choice = ["rock", "paper", "scissors"]
 //display scores
 let playerScore = 0;
 let computerScore = 0;
-let gameScore = (playerScore + " - " + computerScore)
-console.log(gameScore)
 
-
+//displays and updates score
+function updateGameScore() {
+    gameScore = `${playerScore} - ${computerScore}`;
+    console.log(gameScore);
+}
 
 
 
@@ -21,62 +23,58 @@ function getComputerChoice() {
     return choice[(Math.floor(Math.random() * choice.length))];
 }
 
-// function game(){}
 
    
-    // all possible plays, displays player & computer choice and round results message
-    function playRound(playerSelection, computerSelection) {
-        playerSelection = getPlayerChoice();
-        computerSelection = getComputerChoice();
-        console.log(playerSelection);
-        console.log(computerSelection);
+// all possible play outcomes, displays player & computer choice and round results message
+function playRound(playerSelection, computerSelection) {
+    playerSelection = getPlayerChoice();
+    computerSelection = getComputerChoice();
+    console.log(playerSelection);
+    console.log(computerSelection);
 
-            if (playerSelection === "rock" && computerSelection === "rock") {
-                return "It's a draw";
-            } else if (playerSelection === "rock" && computerSelection === "scissors") {
-                return "You win! Rock beats scissors";
-            } else if (playerSelection === "rock" && computerSelection === "paper") {
-                return "You lose! Paper beats Rock";
-            } else if (playerSelection === "paper" && computerSelection === "rock") {
-                return "You win! Paper beats Rock";
-            } else if (playerSelection === "paper" && computerSelection === "scissors") {
-                return "You lose! Scissors beats paper";
-            } else if (playerSelection === "paper" && computerSelection === "paper") {
-                return "It's a draw";
-            } else if (playerSelection === "scissors" && computerSelection === "rock") {
-                return "You lose! Rock beats scissors";
-            } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-                return "It's a draw";
-            } else if (playerSelection === "scissors" && computerSelection === "paper") {
-                return "You win! Scissors beats paper";
-            }
-        }
+    if (playerSelection === "rock" && computerSelection === "rock") {
+        return "It's a draw";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats scissors";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats Rock";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats Rock";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beats paper";
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+        return "It's a draw";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats scissors";
+    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        return "It's a draw";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beats paper";
+    }
+}
 
-    // }
 
 
 // adds points based on result of round
 function scoring(roundResult) {
     if (roundResult === "You win! Rock beats scissors" || 
-        roundResult === "You win! Paper beats Rock" || 
-        roundResult === "You win! Scissors beats paper") {
-        playerScore += 1;
+    roundResult === "You win! Paper beats Rock" || 
+    roundResult === "You win! Scissors beats paper") {
+    playerScore += 1;
     } else if (roundResult === "You lose! Paper beats Rock" ||
-        roundResult === "You lose! Scissors beats paper" ||
-        roundResult === "You lose! Rock beats scissors") {
-        computerScore += 1;
+    roundResult === "You lose! Scissors beats paper" ||
+    roundResult === "You lose! Rock beats scissors") {
+    computerScore += 1;
     } else {
 
     }
 }
     
 
-// loop round play 5 times
+// loop round play 5 times amd displays updated scores
 for (let i = 0; i < 5; i++) {
     const roundResult = playRound();
     console.log(roundResult);
     scoring(roundResult);
-
-
-
+    updateGameScore();
 }
