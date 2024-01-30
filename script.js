09b184b7f833c3f36a62007dd484a80a27a78ebe
Antony Petsas <152808269+TKP888@users.noreply.github.com>
2024-01-30 21:05:17 +0000

    got return result of round on screen
    need to fix scoring system

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 //options for player and computer
 const choice = ["rock", "paper", "scissors"]
 
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const text = document.querySelector("#text");
+const playerScoreText = document.querySelector("#playerScore");
+const computerScoreText = document.querySelector("#computerScore");
+
 //display scores
 let playerScore = 0;
 let computerScore = 0;
 
-//displays and updates score
-function updateGameScore() {
-    gameScore = `${playerScore} - ${computerScore}`;
-    console.log(gameScore);
-}
 
 
 
-// random selection of 3 choices for player
-function getPlayerChoice()  {
-    return choice[(Math.floor(Math.random() * choice.length))];
+
+
+
+rock.onclick = function() {
+    playRound("rock");
+};
+
+paper.onclick = function() {
+    playRound("paper");
+};
+
+scissors.onclick = function() {
+    playRound("scissors");
+};
+
+
+
+
+
+// //displays and updates score
+// function updateGameScore() {
+//     gameScore = `${playerScore} - ${computerScore}`;
+//     console.log(gameScore);
+// }
+
+function playerChoice()  {
+    const choices = ['rock', 'paper', 'scissors'];
 }
 
 // computer to randomly select one of the 3 choice options
@@ -24,57 +50,38 @@ function getComputerChoice() {
 }
 
 
-   
-// all possible play outcomes, displays player & computer choice and round results message
-function playRound(playerSelection, computerSelection) {
-    playerSelection = getPlayerChoice();
-    computerSelection = getComputerChoice();
-    console.log(playerSelection);
-    console.log(computerSelection);
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+    console.log(`Player chose: ${playerSelection}`);
+    console.log(`Computer chose: ${computerSelection}`);
 
-    if (playerSelection === "rock" && computerSelection === "rock") {
-        return "It's a draw";
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You win! Rock beats scissors";
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You lose! Paper beats Rock";
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win! Paper beats Rock";
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You lose! Scissors beats paper";
-    } else if (playerSelection === "paper" && computerSelection === "paper") {
-        return "It's a draw";
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You lose! Rock beats scissors";
-    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        return "It's a draw";
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You win! Scissors beats paper";
-    }
-}
-
-
-
-// adds points based on result of round
-function scoring(roundResult) {
-    if (roundResult === "You win! Rock beats scissors" || 
-    roundResult === "You win! Paper beats Rock" || 
-    roundResult === "You win! Scissors beats paper") {
-    playerScore += 1;
-    } else if (roundResult === "You lose! Paper beats Rock" ||
-    roundResult === "You lose! Scissors beats paper" ||
-    roundResult === "You lose! Rock beats scissors") {
-    computerScore += 1;
+    if (playerSelection === computerSelection) {
+      text.innerText = "It's a draw";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        text.innerText = `You win! ${playerSelection} beats ${computerSelection}`;
+        playerScore += 1;
+        playerScoreText.innerText += playerScore;
     } else {
-
+        text.innerText = `You lose! ${computerSelection} beats ${playerSelection}`;
+        computerScore += 1;
+        computerScoreText.innerText += computerScore;
     }
-}
-    
+// // adds points based on result of round
+// function scoring(roundResult) {
+//     if (roundResult === "You win! Rock beats scissors" || 
+//     roundResult === "You win! Paper beats Rock" || 
+//     roundResult === "You win! Scissors beats paper") {
+//     playerScore += 1;
+//     } else if (roundResult === "You lose! Paper beats Rock" ||
+//     roundResult === "You lose! Scissors beats paper" ||
+//     roundResult === "You lose! Rock beats scissors") {
+//     computerScore += 1;
+//     } else {
 
-// loop round play 5 times amd displays updated scores
-for (let i = 0; i < 5; i++) {
-    const roundResult = playRound();
-    console.log(roundResult);
-    scoring(roundResult);
-    updateGameScore();
+//     }
+// }
 }
